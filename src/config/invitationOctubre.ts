9ad5bac780d00b78a35a6eb52d4ce907128c8d35
AleGCarranza <ale.ga.ca90@gmail.com>
2026-09-12 22:17:03 -0600

    october invitation fix image october

--- src/config/invitationOctubre.ts
+++ src/config/invitationOctubre.ts
@@ -3,6 +3,6 @@ import { invitation } from "./invitation";
 export const invitationOctubre = {
   ...invitation,
   descripcion:
-    "Acompananos a celebrar el Baby Shower en honor a Regina. Domingo 18 de octubre de 2026, 12:30 P.M.",
+    "Acompananos a celebrar el Baby Shower en honor a Regina. Sabado 3 de octubre de 2026, 12:30 P.M.",
   imagen: "/octubre.png",
 };
